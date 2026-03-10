e4fe3e003752d9903832da023bc5a1567f3bb597
Deep <pk114741@gmail.com>
2026-03-10 15:55:10 +0530

    program

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import AppRouter from './components/Router'  // Direct import
+import {Analytics} from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRouter />  {/* No extra BrowserRouter here */}
+    <Analytics />
   </React.StrictMode>,
 )
